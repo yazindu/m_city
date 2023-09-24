95377cb0a0e97ff1d23a618d7d358dc899bb2bef
yasindu <yazindu@gmail.com>
2023-09-24 10:45:25 +0530

    wip: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-function App() {
+import {Routes, Route, BrowserRouter} from 'react-router-dom'
+import {Header} from "./components/header_footer/Header.tsx";
+import {Home} from "./components/home";
+import {Footer} from "./components/header_footer/Footer.tsx";
+
+const App = () => {
     return (
-        <>
-            Hi
-        </>
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path={'/'} element={<Home/>}/>
+            </Routes>
+            <Footer/>
+        </BrowserRouter>
     )
 }
 
