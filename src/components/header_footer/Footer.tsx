2fc1f11eab9c3b9ce313e9f57dc7a3be95676a8e
yasindu <yazindu@gmail.com>
2023-09-24 14:31:10 +0530

    feat: footer

--- src/components/header_footer/Footer.tsx
+++ src/components/header_footer/Footer.tsx
@@ -1,7 +1,14 @@
+import {CityLogo} from "../utils/tools.tsx";
+
 export const Footer = () => {
     return (
-        <>
-            <h1>Footer</h1>
-        </>
+        <footer className={'bck_blue'}>
+            <div>
+                <CityLogo link={true} linkTo={'/'} width={'70px'} height={'70px'}/>
+            </div>
+            <div className={'footer_discl'}>
+                Manchester city 2021. All rights reserved.
+            </div>
+        </footer>
     )
 }
