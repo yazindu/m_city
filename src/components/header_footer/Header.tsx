16d781b993cca1a5d6612cc5a713117d186a0d90
yasindu <yazindu@gmail.com>
2023-09-24 13:50:15 +0530

    feat: header

--- src/components/header_footer/Header.tsx
+++ src/components/header_footer/Header.tsx
@@ -1,7 +1,34 @@
+import {AppBar, Toolbar, Button} from '@mui/material/'
+import {Link} from "react-router-dom";
+
 export const Header = () => {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <AppBar
+            position={'fixed'}
+            style={{
+                backgroundColor: '#98c5e9',
+                boxShadow: 'none',
+                padding: '10px 0',
+                borderBottom: '2px solid #00285e'
+            }}
+        >
+            <Toolbar style={{display: 'flex'}}>
+                <div style={{flexGrow: 1}}>
+                    <div>
+                        logo
+                    </div>
+                </div>
+
+                <Link to={'/the_team'}>
+                    <Button color={"inherit"}>The team</Button>
+                </Link>
+                <Link to={'/the_matches'}>
+                    <Button color={"inherit"}>Matches</Button>
+                </Link>
+                <Link to={'/dashboard'}>
+                    <Button color={"inherit"}>Dashboard</Button>
+                </Link>
+            </Toolbar>
+        </AppBar>
     )
 }
