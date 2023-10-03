36c764f9b299fd76aaab66832f816532674c0923
yasindu <yazindu@gmail.com>
2023-10-03 21:28:01 +0530

    feat: react-move part 2

--- src/components/home/featured/test.tsx
+++ src/components/home/featured/test.tsx
@@ -7,25 +7,63 @@ export const Test = () => {
     const [bck, setBck] = useState('#ffffff')
     return (
         <>
+            <button onClick={()=> setBck('#f44336')}>Update</button>
+            <button onClick={()=> setShow(false)}>Remove</button>
+            <button onClick={()=> setShow(true)}>Show</button>
             <Animate
                 show={show}
                 start={{
                     backgroundColor: bck,
                     width: 500,
                     height: 500,
-                    opacity: 1
+                    opacity: 0
                 }}
                 enter={{
                     backgroundColor: bck,
                     width: [100],
                     height: [100],
-                    opacity: 1,
+                    opacity: [1],
                     timing: {
                         duration: 1000,
                         delay: 1000,
                         ease:easePolyOut
                     }
                 }}
+                update={{
+                    backgroundColor: bck,
+                    opacity: [0.5],
+                    timing: {
+                        duration: 2000,
+                        ease:easePolyOut
+                    },
+                    events:{
+                        start : ()=> {
+                            console.log('STARTED')
+                        },
+                        end : ()=> {
+                            console.log('ENDED')
+                        },
+                        interrupt : ()=> {
+                            console.log('INTERRUPTED')
+                        }
+                    }
+                }}
+                leave={[
+                    {
+                        width: [1000],
+                        timing: {
+                            duration: 500,
+                            ease:easePolyOut
+                        }
+                    },{
+                        opacity: [0],
+                        timing: {
+                            delay: 2000,
+                            duration: 3000,
+                            ease:easePolyOut
+                        }
+                    }
+                ]}
             >{
                 ({width, height, opacity, backgroundColor}) => (
                     <div style={{
