aa448e786653a0b5fdedbc85bd37a8b34906c94a
yasindu <yazindu@gmail.com>
2023-10-10 14:44:58 +0530

    feat: Finished the Email promotion form

--- src/components/home/promotion/Enroll.tsx
+++ src/components/home/promotion/Enroll.tsx
@@ -3,6 +3,9 @@ import {useFormik} from "formik";
 import * as Yup from 'yup'
 import {Fade} from "react-awesome-reveal";
 import {CircularProgress} from "@mui/material";
+import {promotionsCollection} from "../../../config/firebase_config.ts";
+import {addDoc, getDocs, FirestoreError, query, where} from "firebase/firestore";
+import {showToastError, showToastSuccess} from "../../utils/tools.tsx";
 
 export const Enroll = () => {
     const [loading, setLoading] = useState(false);
@@ -16,9 +19,31 @@ export const Enroll = () => {
         }),
         onSubmit: (values) => {
             setLoading(true);
-            //submit form
+            submitForm(values)
         }
     })
+
+    const submitForm = async ({email}: { email: string }) => {
+        const q = query(promotionsCollection, where("email", "==", email));
+        try {
+            const querySnapshot = await getDocs(q);
+            if (querySnapshot.size >= 1) {
+                showToastError('Sorry you are already in the list :(')
+                setLoading(false);
+                return false;
+            } else {
+                await addDoc(promotionsCollection, {email: email});
+                formik.resetForm()
+                showToastSuccess('Congratulations! :)')
+                setLoading(false);
+            }
+        } catch (error) {
+            const e = error as FirestoreError
+            setLoading(false);
+            showToastError(e.message)
+        }
+    }
+
     return (
         <Fade>
             <div className={'enroll_wrapper'}>
